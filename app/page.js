9cefe7cbc16a6ca8aa060214cd98f2296533b88f
Tomas Martin <tomassmartin04@gmail.com>
2025-01-21 14:07:19 -0300

    feat: video

--- app/page.js
+++ app/page.js
@@ -129,7 +129,7 @@ export default function Home() {
               </p>
               <a
                 className="text-amber-600 text-xl py-2 lg:flex justify-center"
-                href=""
+                href="https://www.youtube.com/watch?v=IjnFaRVMz5M"
               >
                 <AiOutlineLink />
               </a>
